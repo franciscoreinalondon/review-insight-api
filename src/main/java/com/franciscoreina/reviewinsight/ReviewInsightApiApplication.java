8cc83e717af715ceec4356ac5410f7b18b1e51d4
Francisco Reina <franciscoreinaponce@gmail.com>
2026-04-25 11:36:31 +0100

    Initial commit

---
+++ src/main/java/com/franciscoreina/reviewinsight/ReviewInsightApiApplication.java
@@ -0,0 +1,13 @@
+package com.franciscoreina.reviewinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewInsightApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewInsightApiApplication.class, args);
+    }
+
+}
